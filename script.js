2732705d51bcf5de6c1ad302e2d3d6c432f3640e
Glitch (glitch-hello-website) <none>
2023-01-06 00:58:39 +0000

    🍖🍜 Checkpoint ./style.css:24111152/33 ./index.html:24111152/13 ./script.js:24111152/14971

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ const playerLives = 6;
 
 //Link text 
 playerLivesCount.textContent = playerLives;
+
+//Generate the data
+const getData = () => [
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/peach-prc1.jpg?v=1672965353006", name: "peach prc"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/chloe-moriondo1.jpeg?v=1672965374975", name: "chloe moriondo"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/chase-atlantic1.jpg?v=1672965387477", name: "chase atlantic"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/nbhd-jesse1.jpg?v=1672965383393", name: "the nbhd"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/cavetown1.jpg?v=1672965393066", name: "cavetown"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/brakence1.jpg?v=1672965400604", name: "brakence"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/ariana-grande1.jpg?v=1672965409099", name: "ariana grande"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/ashnikko1.jpg?v=1672965497504", name: "ashnikko"},
+  
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/peach-prc1.jpg?v=1672965353006", name: "peach prc"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/chloe-moriondo1.jpeg?v=1672965374975", name: "chloe moriondo"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/chase-atlantic1.jpg?v=1672965387477", name: "chase atlantic"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/nbhd-jesse1.jpg?v=1672965383393", name: "the nbhd"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/cavetown1.jpg?v=1672965393066", name: "cavetown"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/brakence1.jpg?v=1672965400604", name: "brakence"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/ariana-grande1.jpg?v=1672965409099", name: "ariana grande"},
+  {imgSrc: "https://cdn.glitch.global/31d77a6b-cc56-4a97-a436-3bed549e6b3b/ashnikko1.jpg?v=1672965497504", name: "ashnikko"},
+];
+
+
+//Randomize data
+
+const randomize = 
